7744d0f5397f9980ac4d9471fbc8c21d05edfd9a
Chayathon <chayathon_p@cmu.ac.th>
2022-12-13 15:34:13 +0700

    lab7_3_COMPLETE

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
-
+int adiff(int a,int b)
+{
+  int x = abs(((360+(b % 360)) % 360)-((360+(a % 360)) % 360));
+  if(x<180)
+  {
+  return x;
+  }
+  else
+  {
+  return 360-x;
+  }
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << "\n";
+  cout << adiff(210,45) << "\n";
+  cout << adiff(0,360) << "\n";
+  cout << adiff(10,350) << "\n";
+  cout << adiff(95,260) << "\n";
+  cout << adiff(90,-90) << "\n";
+  cout << adiff(1000,280) << "\n";
+  cout << adiff(60,244) << "\n";
 }
